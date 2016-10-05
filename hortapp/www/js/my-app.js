109837dc722fad4b964ff5f24763983d6f350294
Tiago Possato <tiago@debian>
2016-10-04 21:41:07 -0300

    Tentando trabalhar com view

--- hortapp/www/js/my-app.js
+++ hortapp/www/js/my-app.js
@@ -11,16 +11,22 @@ var myApp = new Framework7({
 // We need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
 
+var $ = Framework7.$;
+
 // Add view
 var mainView = myApp.addView('.view-main', {
     // Because we want to use dynamic navbar, we need to enable it for this view:
     dynamicNavbar: true
 });
 
+// var salvarPerfil = myApp.addView('.salvar-perfil', {
+//     dynamicNavbar: true
+// });
+
 // Now we need to run the code that will be executed only for About page.
 
 // Option 1. Using page callback for page (for "about" page in this case) (recommended way):
-myApp.onPageInit('about', function(page) {
+myApp.onPageInit('chat', function(page) {
     // Do something here for "about" page
     //myApp.alert('è nois');
 });
