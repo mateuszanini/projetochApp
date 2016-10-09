4ac93595a72d7c1f77955e579b7b6e1e22065ca4
Mateus Zanini <mateus-zanini@hotmail.com>
2016-10-09 20:45:33 -0300

    Implementando a busca por CEP

--- hortapp/www/js/my-app.js
+++ hortapp/www/js/my-app.js
@@ -6,49 +6,21 @@ var myApp = new Framework7({
     scrollTopOnNavbarClick: true,
     animateNavBackIcon: true,
     swipePanel: 'left',
+    /*NOTIFICATION*/
+    notificationTitle: "HortApp",
+    notificationHold: 5000,
+    notificationCloseOnClick: false
 });
 
-// We need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
-
 var $ = Framework7.$;
 
-// Add view
 var mainView = myApp.addView('.view-main', {
-    // Because we want to use dynamic navbar, we need to enable it for this view:
     dynamicNavbar: true
 });
 
-myApp.onPageBeforeInit('meuPerfil', function(page){
-  listaEstados();
-  listaCidades();
+myApp.onPageInit('meuPerfil', function(page){
+  usuario.initialize();
+  usuario.listaEstados();
+  usuario.listaCidades();
 });
-
-/*var toolbarDefault = '<a href="views/chat/chat.html">' +
-    '<i class="fa fa-comments" aria-hidden="true"></i>' +
-    '<span>Chat</span>' +
-    '</a>' +
-    '<a href="views/usuario/perfil.html" class="link">' +
-    '<i class="fa fa-apple" aria-hidden="true"></i>' +
-    '<span>Apple</span>' +
-    '</a>' +
-    '<a href="#" class="link">' +
-    '<i class="fa fa-clock-o" aria-hidden="true"></i>' +
-    '<span>Lembrete</span>' +
-    '</a>';
-
-myApp.onPageBeforeInit('perfil', function(page) {
-    // Do something here for "about" page
-    //myApp.alert('è nois');
-    $$('.toolbar-inner').html(
-        '<a href="#" class="link">' +
-        '<i class="fa fa-check" aria-hidden="true"></i>' +
-        '<span>Salvar</span>' +
-        '</a>');
-});
-
-myApp.onPageBack('perfil', function(page) {
-    // Do something here for "about" page
-    //myApp.alert('è nois');
-    $$('.toolbar-inner').html(toolbar);
-});*/
--- hortapp/www/js/usuario/usuario.js
+++ hortapp/www/js/usuario/usuario.js
@@ -1,30 +1,74 @@
-function listaEstados() {
-    var strUf = '';
-    for (var i = 0; i < uf.length; i++) {
-        if (uf[i].ufSigla == "SC") {
-            strUf += '<option value="' + uf[i].ufSigla + '" selected>' + uf[i].ufNome + '</option>';
-            $("#estadoSelecionado").html(uf[i].ufNome);
-        } else {
-            strUf += '<option value="' + uf[i].ufSigla + '">' + uf[i].ufNome + '</option>';
-        }
-    }
-    $("#uf").html(strUf);
-}
-
-function listaCidades() {
-    var ufSelecionado = $("#uf").val();
-    var strCidade = '';
-    for (var i = 0; i < uf.length; i++) {
-        if (uf[i].ufSigla == ufSelecionado) {
-            for (var j = 0; j < uf[i].cidades.length; j++) {
-                if (j === 0) {
-                    strCidade += '<option value="' + uf[i].cidades[j].cidCodigo + '" selected>' + uf[i].cidades[j].cidNome + '</option>';
-                    $("#cidadeSelecionada").html(uf[i].cidades[j].cidNome);
+var usuario = {
+    initialize: function() {
+        $("#usuLocalizacaoAtual").change(function() {
+            usuario.mostraMapa();
+        });
+        $("#ufCodigo").change(function() {
+            usuario.listaCidades();
+        });
+        $("#usuCep").keyup(function() {
+            var cep = $(this).val().replace(/\D/g, '');
+            if (cep.length == 8) {
+                myApp.showIndicator();
+                var validacep = /^[0-9]{8}$/;
+                if (validacep.test(cep)) {
+                    $.getJSON("//viacep.com.br/ws/" + cep + "/json/?callback=?", function(dados) {
+                        if (!("erro" in dados)) {
+                            console.log(dados);
+                        } else {
+                            myApp.addNotification({
+                                title: 'CEP não encontrado',
+                                message: 'Por favor, digite um CEP existente.'
+                            });
+                        }
+                        myApp.hideIndicator();
+                    });
                 } else {
-                    strCidade += '<option value="' + uf[i].cidades[j].cidCodigo + '">' + uf[i].cidades[j].cidNome + '</option>';
+                    alert("CEP Inválido");
+                }
+            }
+        });
+    },
+
+    mostraMapa: function() {
+        $('.divisor').toggleClass('animated zoomOut');
+        $('.divisor').toggleClass('animated zoomIn');
+        $('.mapa').toggleClass('hide');
+        $('.mapa').toggleClass('animated zoomIn');
+        /*if ($("#usuLocalizacaoAtual").is(":checked")) {
+        } else {
+        }*/
+    },
+
+    listaEstados: function() {
+        var strUf = '';
+        for (var i = 0; i < uf.length; i++) {
+            if (uf[i].ufSigla == "SC") {
+                strUf += '<option value="' + uf[i].ufCodigo + '" selected>' + uf[i].ufNome + '</option>';
+                $("#estadoSelecionado").html(uf[i].ufNome);
+            } else {
+                strUf += '<option value="' + uf[i].ufCodigo + '">' + uf[i].ufNome + '</option>';
+            }
+        }
+        $("#ufCodigo").html(strUf);
+
+    },
+
+    listaCidades: function() {
+        var ufSelecionado = parseInt($("#ufCodigo").val());
+        var strCidade = '';
+        for (var i = 0; i < uf.length; i++) {
+            if (uf[i].ufCodigo == ufSelecionado) {
+                for (var j = 0; j < uf[i].cidades.length; j++) {
+                    if (j === 0) {
+                        strCidade += '<option value="' + uf[i].cidades[j].cidCodigo + '" selected>' + uf[i].cidades[j].cidNome + '</option>';
+                        $("#cidadeSelecionada").html(uf[i].cidades[j].cidNome);
+                    } else {
+                        strCidade += '<option value="' + uf[i].cidades[j].cidCodigo + '">' + uf[i].cidades[j].cidNome + '</option>';
+                    }
                 }
             }
         }
+        $("#cidCodigo").html(strCidade);
     }
-    $("#cidade").html(strCidade);
-}
+};
