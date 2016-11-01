2efd6a22500a452c1ce10aca2fa15f6954784b06
Mateus Zanini <mateus-zanini@hotmail.com>
2016-11-01 11:21:01 -0200

    Alterando tela de Preferências. Mudar para page ao invés de popup

--- hortapp/www/js/usuario/preferencias.js
+++ hortapp/www/js/usuario/preferencias.js
@@ -1,5 +1,6 @@
 $('#usuSalvarPreferencias').click(function() {
-    myApp.alert("Preferências salvas!");
+    var formData = myApp.formToJSON('#usuPreferenciasForm');
+    console.log(JSON.stringify(formData));
 });
 
 var today = new Date();
@@ -8,9 +9,28 @@ var calendarDateFormat = myApp.calendar({
     input: '#usuPreferenciasData',
     dateFormat: 'DD, dd de MM de yyyy',
     toolbarCloseText: 'Concluído',
+    closeOnSelect: true,
     minDate: new Date().setDate(today.getDate() - 1),
     monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
     monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
     dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
     dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab']
 });
+
+$("#usuPreferenciasDistancia").on("input change", function() {
+    preferencias.valorDistancia();
+});
+
+
+var preferencias = {
+    initialize: function() {
+        preferencias.valorDistancia();
+    },
+    valorDistancia: function() {
+        var distancia = parseFloat($("#usuPreferenciasDistancia").val());
+        $("#distancia").html(distancia.toFixed(1) + " Km");
+    },
+
+}
+
+preferencias.initialize();
