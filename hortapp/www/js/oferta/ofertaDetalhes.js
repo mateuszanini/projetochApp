1f079c4e84740e9ad156a2b01fd0dd91648d2428
Mateus Zanini <mateus-zanini@hotmail.com>
2016-11-18 11:06:20 -0200

    Card de detalhes da oferta criado.

--- hortapp/www/js/oferta/ofertaDetalhes.js
+++ hortapp/www/js/oferta/ofertaDetalhes.js
@@ -1,29 +1,43 @@
-//var dadosJSON;
 var ofertaDetalhes = function(oftCodigo) {
     ofertaController.getById(oftCodigo, function(dados) {
-        //dadosJSON = JSON.stringify(dados);
-        //dadosJSON = dados;
         console.log(dados);
+
         dados.usuario['usuImagem'] = "https://lh5.googleusercontent.com/-2SJyjwyB0aA/AAAAAAAAAAI/AAAAAAAAAl4/b6LstKRYF1U/s64/photo.jpg";
         dados.usuario['usuNome'] = "Mateus Zanini";
-        console.log(dados.usuario['usuImagem']);
-        var caminhoImg = config.enderecoImagem + dados.oferta['oftImagem'];
-        $("#imgMiniatura").attr("src", caminhoImg);
+
         $("#usuarioAvatar").attr("src", dados.usuario['usuImagem']);
         $(".facebook-name").html(dados.usuario['usuNome']);
 
-        /*=== Default standalone ===*/
-        var myPhotoBrowserStandalone = myApp.photoBrowser({
-            photos : [{
-                url: caminhoImg,
-                caption: "Legenda"
+        var itmNome = ""
+        for (var i = 0; i < itens.length; i++) {
+            if (itens[i].itmCodigo == dados.oferta['itmCodigo']) {
+                itmNome = itens[i].itmNome;
             }
-            ],
+        }
+
+        $("#itmNome").html(itmNome);
+        var caminhoImg = config.enderecoImagem + dados.oferta['oftImagem'];
+        $("#imgMiniatura").attr("src", caminhoImg);
+        $("#oftDataInicial").html(dados.oferta['oftDataInicial']);
+        $("#oftDataFinal").html(dados.oferta['oftDataFinal']);
+        $("#oftQuantidade").html(dados.oferta['oftQuantidade']);
+        $("#oftValor").html("R$ " + dados.oferta['oftValor']);
+
+        var myPhotoBrowserStandalone = myApp.photoBrowser({
+            photos: [{
+                url: caminhoImg,
+                caption: itmNome
+            }],
             theme: 'dark'
         });
-        //Open photo browser on click
-        $('#imgMiniatura').on('click', function () {
+        $('#visualizarImagem').on('click', function() {
             myPhotoBrowserStandalone.open();
+        });
+
+        $('#btnReservar').on('click', function() {
+            myApp.prompt('Qual quantidade você deseja reservar?', function(value) {
+                myScript.notificacao("Sucesso", "Você reservou " + value + " itens!", true);
+            });
         });
 
     });
