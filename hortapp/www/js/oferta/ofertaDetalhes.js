7d49e4e7072b54a3901edcb40e1368a69b661fbe
Mateus Zanini <mateus-zanini@hotmail.com>
2016-11-24 09:19:19 -0200

    Mostrando endereço no mapa na tela de detalhes da oferta

--- hortapp/www/js/oferta/ofertaDetalhes.js
+++ hortapp/www/js/oferta/ofertaDetalhes.js
@@ -2,11 +2,12 @@ var ofertaDetalhes = function(oftCodigo) {
     ofertaController.getById(oftCodigo, function(dados) {
         console.log(dados);
 
-        dados.usuario['usuImagem'] = "https://lh5.googleusercontent.com/-2SJyjwyB0aA/AAAAAAAAAAI/AAAAAAAAAl4/b6LstKRYF1U/s64/photo.jpg";
-        dados.usuario['usuNome'] = "Mateus Zanini";
+        //dados.usuario['usuImagem'] = "https://lh5.googleusercontent.com/-2SJyjwyB0aA/AAAAAAAAAAI/AAAAAAAAAl4/b6LstKRYF1U/s64/photo.jpg";
+        //dados.usuario['usuNome'] = "Mateus Zanini";
 
         $("#usuarioAvatar").attr("src", dados.usuario['usuImagem']);
         $(".facebook-name").html(dados.usuario['usuNome']);
+        $(".facebook-date").html(dados.usuario['usuEmail']);
 
         var itmNome = ""
         for (var i = 0; i < itens.length; i++) {
@@ -21,7 +22,11 @@ var ofertaDetalhes = function(oftCodigo) {
         $("#oftDataInicial").html(dados.oferta['oftDataInicial']);
         $("#oftDataFinal").html(dados.oferta['oftDataFinal']);
         $("#oftQuantidade").html(dados.oferta['oftQuantidade']);
-        $("#oftValor").html("R$ " + dados.oferta['oftValor']);
+        if (dados.oferta['oftValor'] == undefined || dados.oferta['oftValor'] == 0) {
+            $("#oftValor").html("Grátis");
+        } else {
+            $("#oftValor").html("R$ " + dados.oferta['oftValor']);
+        }
 
         var myPhotoBrowserStandalone = myApp.photoBrowser({
             photos: [{
@@ -34,11 +39,58 @@ var ofertaDetalhes = function(oftCodigo) {
             myPhotoBrowserStandalone.open();
         });
 
+        //ENDEREÇO
+        ofertaDetalhes.localizacao = new Localizacao();
+
+        ofertaDetalhes.localizacao.mapCanvas =
+            document.getElementById("mapCanvasOferta");
+
+        if (dados.endereco['endLatitude'] && dados.endereco['endLongitude']) {
+            ofertaDetalhes.localizacao.divEnderecoAtual =
+                document.getElementById("usuLocalizacaoOferta");
+
+            ofertaDetalhes.localizacao.geocodeLatLng({
+                lat: dados.endereco['endLatitude'],
+                lng: dados.endereco['endLongitude']
+            });
+        } else {
+            var endereco = "";
+
+            endereco += dados.endereco['endLogradouro'] != undefined ? dados.endereco['endLogradouro'] + ", " : "";
+            endereco += dados.endereco['endNumero'] != undefined ? dados.endereco['endNumero'] + " - " : "";
+            endereco += dados.endereco['endBairro'] != undefined ? dados.endereco['endBairro'] + ", " : "";
+
+            for (var i = 0; i < uf.length; i++) {
+                if (uf[i].ufCodigo == dados.endereco['ufCodigo']) {
+                    for (var j = 0; j < uf[i].cidades.length; j++) {
+                        if (uf[i].cidades[j].cidCodigo == dados.endereco['cidCodigo']) {
+                            endereco += uf[i].cidades[j].cidNome + " - " + uf[i].ufSigla + ", ";
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+            endereco += dados.endereco['endCep'] != undefined ? dados.endereco['endCep'] + ", Brasil" : "";
+
+            $('#usuLocalizacaoOferta').html('<p class="text-center"><i class="fa fa-map-signs" aria-hidden="true"></i>&nbsp ' +
+            endereco + '</p>');
+
+            ofertaDetalhes.localizacao.geocodeAddress(endereco);
+        }
+
+
+
+        ofertaDetalhes.localizacao.initMap();
+
+        $("#mapCanvasOferta").css("pointer-events", "none");
+        //ofertaDetalhes.localizacao.mapa.removeListener('click');
+
+
         $('#btnReservar').on('click', function() {
             myApp.prompt('Qual quantidade você deseja reservar?', function(value) {
                 myScript.notificacao("Sucesso", "Você reservou " + value + " itens!", true);
             });
         });
-
     });
 };
